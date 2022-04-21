18232a94158484e22bfa21cb0261bd716d6ff110
João Ygo <joaoygo.freitas25@gmail.com>
2022-04-21 10:24:38 -0300

    Gênesis

---
+++ android/app/src/main/kotlin/com/example/cafeteira_ygo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cafeteira_ygo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/core/routes/app_pages.dart
@@ -0,0 +1,5 @@
+import 'package:get/get_navigation/get_navigation.dart';
+
+class AppPages {
+  static final List<GetPage> pages = [];
+}
